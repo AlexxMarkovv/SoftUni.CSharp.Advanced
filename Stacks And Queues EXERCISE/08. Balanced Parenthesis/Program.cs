700ff41388a8284718083a9f2e3fc5424a0812d5
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-04 23:08:23 +0200

    Task

---
+++ Stacks And Queues EXERCISE/08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _08._Balanced_Parenthesis
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+
+            Stack<char> stack = new();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                switch (expression[i])
+                {
+                    case '(':
+                    case '{':
+                    case '[':
+                        stack.Push(expression[i]);
+                        break;
+                    case ')':
+                        if (stack.Count == 0 || stack.Pop() != '(')
+                        {
+                            Console.WriteLine("NO");
+                            return;
+                        }
+                        break;
+                    case '}':
+                        if (stack.Count == 0 || stack.Pop() != '{')
+                        {
+                            Console.WriteLine("NO");
+                            return;
+                        }
+                        break;
+                    case ']':
+                        if (stack.Count == 0 || stack.Pop() != '[')
+                        {
+                            Console.WriteLine("NO");
+                            return;
+                        }
+                        break;
+                }
+            }
+
+            if (stack.Count > 0)
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine("YES");
+            }
+        }
+    }
+}
