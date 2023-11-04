33985f8a38e7af2c7f290c58839da69b7f46de23
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-04 23:01:47 +0200

    Added Tasks

---
+++ Stacks And Queues EXERCISE/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _01._Basic_Stack_Operations
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsInfo = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] listOfIntegers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < numsInfo[0]; i++)
+            {
+                stack.Push(listOfIntegers[i]);
+            }
+
+            for (int i = 0; i < numsInfo[1]; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Count > 0)
+            {
+                if (stack.Contains(numsInfo[2]))
+                {
+                    Console.WriteLine("true");
+                }
+                else if (!stack.Contains(numsInfo[2]))
+                {
+                    int smallestNum = stack.Min();
+                    Console.WriteLine(smallestNum);
+                }
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
