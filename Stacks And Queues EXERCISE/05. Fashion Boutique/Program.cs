55a2bc3315e359353aca28d79c4ce8a028b42227
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-04 23:05:51 +0200

    Task

---
+++ Stacks And Queues EXERCISE/05. Fashion Boutique/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _05._Fashion_Boutique
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rackCapacity = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>(clothes);
+            int usedRacks = 1;
+            int nextRack = rackCapacity;
+
+            while (stack.Count > 0)
+            {
+                int currCloth = stack.Peek();
+                if (rackCapacity >= currCloth)
+                {
+                    stack.Pop();
+                    rackCapacity -= currCloth;
+                }
+                else
+                {
+                    rackCapacity = nextRack;
+                    usedRacks++;
+                }
+            }
+            Console.WriteLine(usedRacks);
+        }
+    }
+}
