7902357daa45b04c229ed7e1a25d8be871b60713
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-04 23:06:20 +0200

    Task

---
+++ Stacks And Queues EXERCISE/06. Songs Queue/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _06._Songs_Queue
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = Console.ReadLine().Split(", ");
+
+            Queue<string> songsQueue = new Queue<string>(songs);
+
+            string command = Console.ReadLine();
+            while (true)
+            {
+                if (songsQueue.Count == 0)
+                {
+                    Console.WriteLine("No more songs!");
+                    break;
+                }
+
+                if (command == "Play" && songsQueue.Count > 0)
+                {
+                    songsQueue.Dequeue();
+                }
+                else if (command == "Show" && songsQueue.Count > 0)
+                {
+                    Console.WriteLine(string.Join(", ", songsQueue));
+                }
+
+                if (command.StartsWith("Add"))
+                {
+                    string song = command.Substring(4);
+                    if (!songsQueue.Contains(song))
+                    {
+                        songsQueue.Enqueue(song);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
