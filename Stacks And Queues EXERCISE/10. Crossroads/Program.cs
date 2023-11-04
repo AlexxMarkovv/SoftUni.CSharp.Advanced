326771b135a3fc3078ab032c3a46c28b1bb80e4f
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-04 23:09:46 +0200

    Task

---
+++ Stacks And Queues EXERCISE/10. Crossroads/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace _10._Crossroads
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int greenLightTime = int.Parse(Console.ReadLine());
+            int freeWindowTime = int.Parse(Console.ReadLine());
+
+            int lightTime = greenLightTime;
+            int freeTime = freeWindowTime;
+
+            Queue<char> queue = new Queue<char>();
+            int totalCarsPassed = 0;
+
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+                if (command == "green")
+                {
+                    greenLightTime = lightTime;
+                    freeWindowTime = freeTime;
+                }
+                else if (greenLightTime > 0)
+                {
+                    for (int i = 0; i < command.Length; i++)
+                    {
+                        queue.Enqueue(command[i]);
+                    }
+
+                    while (queue.Count > 0)
+                    {
+                        char lastChar = queue.Peek();
+                        if (greenLightTime > 0)
+                        {
+                            queue.Dequeue();
+                            greenLightTime--;
+                        }
+                        else if (freeWindowTime > 0)
+                        {
+                            queue.Dequeue();
+                            freeWindowTime--;
+                        }
+                        else
+                        {
+                            Console.WriteLine("A crash happened!");
+                            Console.WriteLine($"{command} was hit at {lastChar}.");
+                            return;
+                        }
+                    }
+
+                    totalCarsPassed++;
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine($"{totalCarsPassed} total cars passed the crossroads.");
+
+        }
+    }
+}
