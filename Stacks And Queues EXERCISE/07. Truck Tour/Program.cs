c11dac478ac014517dca872407baae618ed710b4
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-04 23:06:44 +0200

    Task

---
+++ Stacks And Queues EXERCISE/07. Truck Tour/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _07._Truck_Tour
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int petrolPumps = int.Parse(Console.ReadLine());
+
+            Queue<int[]> queue = new Queue<int[]>();
+            
+            for (int i = 0; i < petrolPumps; i++)
+            {
+                int[] pumpInfo2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                queue.Enqueue(pumpInfo2);
+            }
+
+            int petrol = 0;
+            int startingIndex = 0;
+
+            for (int i = 0; i < queue.Count; i++)
+            {
+                int[] pumpInfo = queue.Peek();
+                petrol += pumpInfo[0];
+
+                if (petrol >= pumpInfo[1])
+                {
+                    petrol -= pumpInfo[1];
+                }
+                else
+                {
+                    petrol = 0;
+                    queue.Enqueue(queue.Dequeue());
+                    startingIndex++;
+                }
+            }
+
+            Console.WriteLine(startingIndex);
+        }
+    }
+}
