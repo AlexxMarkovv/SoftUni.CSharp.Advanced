fcc2045668e7f267cd397549284155f8453027fa
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-04 23:03:36 +0200

    Added Tasks

---
+++ Stacks And Queues EXERCISE/2. Basic Queue Operations/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _2._Basic_Queue_Operations
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsInfo = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] listOfIntegers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> numbersInQueue = new Queue<int>();
+            for (int i = 0; i < numsInfo[0]; i++)
+            {
+                numbersInQueue.Enqueue(listOfIntegers[i]);
+            }
+
+            for (int i = 0; i < numsInfo[1]; i++)
+            {
+                numbersInQueue.Dequeue();
+            }
+
+            if (numbersInQueue.Count > 0)
+            {
+                if (numbersInQueue.Contains(numsInfo[2]))
+                {
+                    Console.WriteLine("true");
+                }
+                else if (!numbersInQueue.Contains(numsInfo[2]))
+                {
+                    int smallestNum = numbersInQueue.Min();
+                    Console.WriteLine(smallestNum);
+                }
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
---
+++ Stacks And Queues EXERCISE/3. Maximum and Minimum Element/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _3._Maximum_and_Minimum_Element
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            int numOfQueries = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numOfQueries; i++)
+            {
+                int[] queries = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int typeOfQuery = queries[0];
+
+                if (typeOfQuery == 1)
+                {
+                    stack.Push(queries[1]);
+                }
+                else if (typeOfQuery == 2 && stack.Count > 0)
+                {
+                    stack.Pop();
+                }
+
+                if (typeOfQuery == 3 && stack.Count > 0)
+                {
+                    int maxNum = stack.Max();
+                    Console.WriteLine(maxNum);
+                }
+                else if (typeOfQuery == 4 && stack.Count > 0)
+                {
+                    int minNum = stack.Min();
+                    Console.WriteLine(minNum);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}
