f262cf7caae7343b2853ea12e7a1c1ff4a139f4a
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-05 19:31:11 +0200

    Create Program.cs

---
+++ Sets and Dictionaries Advanced LAB/03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _03._Largest_3_Numbers
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            array = array.OrderByDescending(x => x)
+                .Take(3).ToArray();
+
+            foreach (var item in array)
+            {
+                Console.Write($"{item} ");
+            }   
+        }
+    }
+}
