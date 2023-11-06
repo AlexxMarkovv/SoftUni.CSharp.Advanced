983238499c643691004e761325b5fc94efac4cf6
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-06 09:53:56 +0200

    Create Program.cs

---
+++ Sets and Dictionaries Advanced LAB/07. Parking Lot/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _07._Parking_Lot
+{
+    using System;
+    using System.Collections.Generic;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            HashSet<string> carNumbers = new HashSet<string>();
+
+            while (input[0] != "END")
+            {
+                string cmndType = input[0];
+                string carNum = input[1];
+
+                if (cmndType == "IN")
+                {
+                    carNumbers.Add(carNum);
+                }
+                else if (cmndType == "OUT" && carNumbers.Contains(carNum))
+                {
+                    carNumbers.Remove(carNum);
+                }
+
+                input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            if (carNumbers.Count > 0)
+            {
+                foreach (string carNum in carNumbers)
+                {
+                    Console.WriteLine(carNum);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
