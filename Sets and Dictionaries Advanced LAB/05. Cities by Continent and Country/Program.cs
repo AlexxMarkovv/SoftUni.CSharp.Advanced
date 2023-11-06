04ae36f7bc5dffb5883cbbd0d717a79e3ae5fcdc
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-06 09:52:52 +0200

    Create Program.cs

---
+++ Sets and Dictionaries Advanced LAB/05. Cities by Continent and Country/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _05._Cities_by_Continent_and_Country
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, List<string>>> continents
+                = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] contData = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string continent = contData[0];
+                string country = contData[1];
+                string city = contData[2];
+
+                if (!continents.ContainsKey(continent))
+                {
+                    continents.Add(continent, new Dictionary<string, List<string>>());
+                }
+
+                if (!continents[continent].ContainsKey(country))
+                {
+                    continents[continent].Add(country, new List<string>());
+                }
+
+                continents[continent][country].Add(city);
+            }
+
+            foreach (var continent in continents)
+            {
+                Console.WriteLine($"{continent.Key}:");
+
+                foreach (var country in continent.Value)
+                {
+                    Console.WriteLine($"  {country.Key} -> {string.Join(", ", country.Value)}");
+                }
+            }
+        }
+    }
+}
