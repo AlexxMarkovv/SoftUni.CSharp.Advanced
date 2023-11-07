707a0a20304da21bdc2a28fe38860e03aac12c37
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-07 22:45:45 +0200

    TAsk

---
+++ Sets and Dictionaries Advanced - EXERCISE/01. Unique Usernames/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _01._Unique_Usernames
+{
+    using System;
+    using System.Collections.Generic;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> set = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string username = Console.ReadLine();
+                set.Add(username);
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, set));
+        }
+    }
+}
