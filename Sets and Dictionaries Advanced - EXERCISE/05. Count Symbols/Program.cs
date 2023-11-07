40da0f616b048ca214d0c60a3f94b72c10f0daeb
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-07 22:48:33 +0200

    Create Program.cs

---
+++ Sets and Dictionaries Advanced - EXERCISE/05. Count Symbols/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _05._Count_Symbols
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char, int> occurences = new SortedDictionary<char, int>();
+
+            string text = Console.ReadLine();
+
+            foreach (var item in text)
+            {
+                if (!occurences.ContainsKey(item))
+                {
+                    occurences.Add(item, 0);
+                }
+                occurences[item]++;
+            }
+
+            foreach (var item in occurences)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
