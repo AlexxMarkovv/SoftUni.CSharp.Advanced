7eea75758cc94e179466f89c75578a1ac06f73e7
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-07 22:48:06 +0200

    Create Program.cs

---
+++ Sets and Dictionaries Advanced - EXERCISE/03. Periodic Table/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _03._Periodic_Table
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            SortedSet<string> elements = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] elArray = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                elements.UnionWith(elArray);
+            }
+
+            Console.WriteLine(string.Join(" ", elements));
+        }
+    }
+}
