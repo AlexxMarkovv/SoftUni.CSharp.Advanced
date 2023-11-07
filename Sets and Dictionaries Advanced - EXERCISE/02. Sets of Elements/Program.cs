a96d98edb371c1304c6d92eaa7ad7f95821d7eba
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-07 22:47:11 +0200

    Create Program.cs

---
+++ Sets and Dictionaries Advanced - EXERCISE/02. Sets of Elements/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _02._Sets_of_Elements
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> set1 = new HashSet<int>();
+            HashSet<int> set2 = new HashSet<int>();
+
+            HashSet<int> uniqueNums = new HashSet<int>();
+
+            int[] setsCounts = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < setsCounts[0]; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                set1.Add(number);
+            }
+
+            for (int i = 0; i < setsCounts[1]; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                set2.Add(number);
+            }
+
+            //foreach (var item in set1)
+            //{
+            //    if (set2.Contains(item))
+            //    {
+            //        uniqueNums.Add(item);
+            //    }
+            //}
+
+            uniqueNums = set1.Intersect(set2).ToHashSet();
+
+            Console.WriteLine(string.Join(" ", uniqueNums));
+        }
+    }
+}
