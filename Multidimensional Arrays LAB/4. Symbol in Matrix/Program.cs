096b2bcdb0b041d0fb9c3b30967453a05d050a0e
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-08 14:13:59 +0200

    Create Program.cs

---
+++ Multidimensional Arrays LAB/4. Symbol in Matrix/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _4._Symbol_in_Matrix
+{
+    using System;
+    using System.Linq;
+    using System.Data;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n,n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string symbols = Console.ReadLine();
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = symbols[j];
+                }
+            }
+
+            char symbolSearched = char.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matrix[i, j] == symbolSearched)
+                    {
+                        Console.WriteLine($"({i}, {j})");
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{symbolSearched} does not occur in the matrix");
+        }
+    }
+}
