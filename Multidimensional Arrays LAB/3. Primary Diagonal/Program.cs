0f43a0389a8a0c0ef52b94032ac35b517f8bc4c7
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-08 14:13:50 +0200

    Create Program.cs

---
+++ Multidimensional Arrays LAB/3. Primary Diagonal/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _3._Primary_Diagonal
+{
+    using System;
+    using System.Linq;
+    using System.Data;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+            int primeSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] rows = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = rows[j];
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                    {
+                        primeSum += matrix[i, j];
+                    }
+                }
+            }
+
+            Console.WriteLine(primeSum);
+        }
+
+        static void PrintMatrix(int[,] matrix, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{matrix[i,j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
