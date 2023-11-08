ea2c389a03f505431685223f22b259190434e682
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-08 14:11:50 +0200

    Task

---
+++ Multidimensional Arrays LAB/1. Sum Matrix Elements/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _1._Sum_Matrix_Elements
+{
+    using System;
+    using System.Linq;
+    using System.Data;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = matrixSize[0];
+            int cols = matrixSize[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] rowData = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            SumTheMatrix(matrix);
+        }
+
+        static void SumTheMatrix(int[,] matrix)
+        {
+            int sum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    sum += matrix[i, j];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
