65e34ea4c2855845c7272d79ad6b8263d45e30d9
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-10 13:56:27 +0200

    Create Program.cs

---
+++ Multidimensional Arrays - EXERCISE/5. Snake Moves/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace _5._Snake_Moves
+{
+    using System;
+    using System.Linq;
+    using System.Data;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+
+            char[,] matrix = new char[rows, cols];
+
+            string text = Console.ReadLine();
+            int length = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                if (row % 2 == 0)
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
+                        if (length >= text.Length)
+                        {
+                            length = 0;
+                        }
+
+                        matrix[row, col] = text[length];
+                        length++;
+                    }
+                }
+                else
+                {
+                    for (int col = matrix.GetLength(1) - 1; col >= 0; col--)
+                    {
+                        if (length >= text.Length)
+                        {
+                            length = 0;
+                        }
+
+                        matrix[row, col] = text[length];
+                        length++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"{matrix[i, j]}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
