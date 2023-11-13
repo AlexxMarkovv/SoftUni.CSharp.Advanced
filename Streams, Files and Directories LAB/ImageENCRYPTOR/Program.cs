04ed43cbdfb1e90802e908d3d23c0f61ab3dcb60
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-13 23:24:22 +0200

    Create Program.cs

---
+++ Streams, Files and Directories LAB/ImageENCRYPTOR/Program.cs
@@ -0,0 +1,53 @@
+﻿string basePath = "../../../Images/";
+int imageNumber = 1;
+
+//while (File.Exists($"{basePath}{imageNumber}.jpg"))
+//{
+//    using (var stream = new FileStream($"{basePath}{imageNumber}.jpg", FileMode.Open))
+//    {
+//        byte[] buffer = new byte[stream.Length]; 
+
+//        stream.Read(buffer, 0, buffer.Length);
+
+//        Console.WriteLine(string.Join(" ", buffer));
+
+//        using (var encryptedStream = 
+//            new FileStream($"{basePath}{imageNumber}.encrypted.jpg", FileMode.OpenOrCreate))
+//        {
+//            for (int i = 0; i < buffer.Length; i++)
+//            {
+//                buffer[i] = (byte)(buffer[i] ^ 157);
+//            }
+
+//            encryptedStream.Write(buffer, 0, buffer.Length);
+//        }
+//    }
+//    imageNumber++;
+//}
+
+
+imageNumber = 1;
+
+while (File.Exists($"{basePath}{imageNumber}.encrypted.jpg"))
+{
+    using (var stream = new FileStream($"{basePath}{imageNumber}.encrypted.jpg", FileMode.Open))
+    {
+        byte[] buffer = new byte[stream.Length];
+
+        stream.Read(buffer, 0, buffer.Length);
+
+        //Console.WriteLine(string.Join(" ", buffer));
+
+        using (var encryptedStream =
+            new FileStream($"{basePath}{imageNumber}.jpg", FileMode.OpenOrCreate))
+        {
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                buffer[i] = (byte)(buffer[i] ^ 157);
+            }
+
+            encryptedStream.Write(buffer, 0, buffer.Length);
+        }
+    }
+    imageNumber++;
+}
