02b620feb1d26fbd4f3bc9636782e128bf295fe0
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-13 23:23:43 +0200

    Create Program.cs

---
+++ Streams, Files and Directories LAB/07. Folder Size/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace FolderSize
+{
+    using System;
+    using System.IO;
+    public class FolderSize
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            long size = GetFolderSize(folderPath);
+
+            File.WriteAllText(outputFilePath, $"{size / 1024} KB");
+        }
+
+        public static long GetFolderSize(string path)
+        {
+            string[] filePaths = Directory.GetFiles(path);
+
+            long size = 0;
+            foreach (string filePath in filePaths)
+            {
+                FileInfo file = new FileInfo(filePath);
+                size += file.Length;
+                Console.WriteLine(filePath);
+            }
+
+            foreach (var dirPath in Directory.GetDirectories(path))
+            {
+                size += GetFolderSize(dirPath);
+            }
+
+            return size;
+        }
+    }
+}
