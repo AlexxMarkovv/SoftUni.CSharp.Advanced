aa38c5023bda35f0ef0a1cb5960a5daae673d028
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-14 21:11:04 +0200

    Create CopyDirectory.cs

---
+++ Streams Files And Directories Exercise/CopyDirectory/CopyDirectory.cs
@@ -0,0 +1,37 @@
+﻿namespace CopyDirectory
+{
+    using System;
+    using System.IO;
+
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath = "C:\\Users\\Lenovo\\OneDrive\\Pictures";  //@$"{Console.ReadLine()}";
+            string outputPath = "../../../output.txt";  /*@$"{Console.ReadLine()}";*/
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            if (File.Exists(outputPath))
+            {
+                Directory.Delete(outputPath, true);
+            }
+
+            Directory.CreateDirectory(outputPath);
+
+            string[] files = Directory.GetFiles(inputPath);
+
+            foreach (string file in files) 
+            {
+                string fileName = Path.GetFileName(file);
+
+                string destination = Path.Combine(outputPath, fileName);
+            
+                File.Copy(file, destination);
+            }
+        }
+    }
+}
