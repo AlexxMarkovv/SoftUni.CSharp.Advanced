102ee6b50cb30c1f3110e4f651ef275db3c64ff9
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-15 21:17:02 +0200

    Create Program.cs

---
+++ Functional Programming LAB/01. Sort Even Numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(n => int.Parse(n))
+    .Where(x => x % 2 == 0)
+    .OrderBy(x => x)
+    .ToArray();
+
+Console.WriteLine(string.Join(", ", numbers));
+    
+
