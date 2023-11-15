abb3c7225a6aecc3e772eb5e057ee8f8020420f0
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-15 21:17:11 +0200

    Create Program.cs

---
+++ Functional Programming LAB/02. Sum Numbers/Program.cs
@@ -0,0 +1,9 @@
+﻿Func<string, int> parser = n => int.Parse(n);
+
+int[] numbers = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(parser)
+    .ToArray();
+
+Console.WriteLine(numbers.Length);
+Console.WriteLine(numbers.Sum());
