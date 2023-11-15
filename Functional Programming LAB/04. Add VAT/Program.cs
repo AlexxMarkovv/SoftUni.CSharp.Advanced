cadad52b225d7356de7e04805161794b90fcb294
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-15 21:17:23 +0200

    Create Program.cs

---
+++ Functional Programming LAB/04. Add VAT/Program.cs
@@ -0,0 +1,11 @@
+﻿decimal[] prices = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(decimal.Parse)
+    .ToArray();
+
+Func<decimal, decimal> func = x => x * 1.20m;
+
+foreach (var price in prices)
+{
+    Console.WriteLine($"{func(price):f2}");
+}
