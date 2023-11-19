a4b6d4c4076cf68e35ea2fb9509895be3da6c734
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-19 14:10:43 +0200

    Create Program.cs

---
+++ Functional Programming - EXERCISE/04. Find Evens or Odds/Program.cs
@@ -0,0 +1,40 @@
+﻿int[] bounds = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+string command = Console.ReadLine();
+
+Func<int, int, List<int>> generateRange = (start, end) =>
+{
+    List<int> range = new();
+
+    for (int i = start; i <= end; i++)
+    {
+        range.Add(i);
+    }
+
+    return range;
+};
+
+Func<string, int, bool> evenOddMAtch = (command, number) =>
+{
+    if(command == "even")
+    {
+        return number % 2 == 0;
+    }
+    else
+    {
+        return number % 2 != 0;
+    }
+};
+
+List<int> numbers = generateRange(bounds[0], bounds[1]);
+
+foreach (var number in numbers)
+{
+    if (evenOddMAtch(command, number))
+    {
+        Console.Write($"{number} ");
+    }
+}
