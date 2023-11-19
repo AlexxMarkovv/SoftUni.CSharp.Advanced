abf0b89ea920abb5afd8ff0e7af441c8f4211adf
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-19 14:10:35 +0200

    Create Program.cs

---
+++ Functional Programming - EXERCISE/03. Custom Min Function/Program.cs
@@ -0,0 +1,21 @@
+﻿HashSet<int> numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(n => int.Parse(n))
+    .ToHashSet();
+
+Func<HashSet<int>, int> minNum = numbers =>
+{
+    int min = int.MaxValue;
+
+    foreach (var number in numbers)
+    {
+        if (number < min)
+        {
+            min = number;
+        }
+    }
+
+    return min;
+};
+
+Console.WriteLine(minNum(numbers));
