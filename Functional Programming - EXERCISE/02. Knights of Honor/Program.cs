b31c5c635d33c9cddb48598422f9363c8ec3e844
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-19 14:10:30 +0200

    Create Program.cs

---
+++ Functional Programming - EXERCISE/02. Knights of Honor/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Action<string> printerWithTitle = name => Console.WriteLine($"Sir {name}");
+
+foreach(string name in names)
+{
+    printerWithTitle(name);
+}
