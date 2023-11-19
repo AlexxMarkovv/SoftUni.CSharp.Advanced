dc48985b7367a6ba7acdbf2e9519b7bc9158611a
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-19 14:11:03 +0200

    Create Program.cs

---
+++ Functional Programming - EXERCISE/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,38 @@
+﻿List<int> numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+int divider = int.Parse(Console.ReadLine());
+
+Func<List<int>, List<int>> reverseNumbers = numbers =>
+{
+    List<int> reversedList = new();
+
+    for (int i = numbers.Count - 1; i >= 0; i--)
+    {
+        reversedList.Add(numbers[i]);
+    }
+
+    return reversedList;
+};
+
+Func<List<int>, Predicate<int>, List<int>> excludesNums = (numbers, match) =>
+{
+    List<int> result = new();
+
+    foreach (var number in numbers)
+    {
+        if (!match(number))
+        {
+            result.Add(number);
+        }
+    }
+
+    return result;
+};
+
+numbers = excludesNums(numbers, number => number % divider == 0);
+numbers = reverseNumbers(numbers);
+
+Console.WriteLine(string.Join(" ", numbers));
