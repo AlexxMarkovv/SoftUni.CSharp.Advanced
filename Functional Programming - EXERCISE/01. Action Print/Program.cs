a0b0fae4956bab1c54472b9d7214d68177eb81b8
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-19 14:10:22 +0200

    Create Program.cs

---
+++ Functional Programming - EXERCISE/01. Action Print/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Action<string> printer = name => Console.WriteLine(name);
+
+foreach (string name in names)
+{
+	printer(name);
+}
