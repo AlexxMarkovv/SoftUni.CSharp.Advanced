aa637aa38b05797bcd6e3b0ded4cdcfa55054a96
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-20 09:12:32 +0200

    Create Program.cs

---
+++ Functional Programming - EXERCISE/11. TriFunction/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Linq;
+
+int charsSum = int.Parse(Console.ReadLine());
+
+string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Func<string, int, bool> checkingCharsSum =
+    (name, sum) => name.Sum(ch => ch) >= charsSum;
+
+Func<string[], int, Func<string, int, bool>, string> getFirstName =
+    (names, charsSum, match) => names.FirstOrDefault(name => match(name, charsSum));
+
+Console.WriteLine(getFirstName(names, charsSum, checkingCharsSum));
+
