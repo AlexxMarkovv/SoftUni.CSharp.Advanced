3754666ad65ac203b2fcd845b59423613ea539a5
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-20 09:12:05 +0200

    Create Program.cs

---
+++ Functional Programming - EXERCISE/08. List Of Predicates/Program.cs
@@ -0,0 +1,36 @@
+﻿int range = int.Parse(Console.ReadLine());
+
+HashSet<int> dividers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToHashSet();
+
+List<Predicate<int>> predicates = new();
+
+int[] numbers = Enumerable.Range(1, range).ToArray();
+
+foreach(int divider in dividers)
+{
+    Predicate<int> check = n => n % divider == 0;
+
+    predicates.Add(check);
+}
+
+foreach(var number in numbers)
+{
+    bool isDivisible = true;
+
+    foreach (var match in predicates)
+    {
+        if (!match(number))
+        {
+            isDivisible = false;
+            break;
+        }
+    }
+
+    if (isDivisible)
+    {
+        Console.Write(number + " ");
+    }
+}
