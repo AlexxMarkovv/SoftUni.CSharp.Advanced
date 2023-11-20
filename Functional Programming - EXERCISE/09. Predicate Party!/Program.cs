87f383013391fe1213a2fef757ba329660cc9ee4
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-20 09:12:16 +0200

    Create Program.cs

---
+++ Functional Programming - EXERCISE/09. Predicate Party!/Program.cs
@@ -0,0 +1,49 @@
+﻿List<string> people = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+string command;
+while ((command = Console.ReadLine()) != "Party!")
+{
+    string[] cmndArrgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    string action = cmndArrgs[0];
+    string filter = cmndArrgs[1];
+    string value = cmndArrgs[2];
+
+    if (action == "Remove")
+    {
+        people.RemoveAll(CheckingNames(filter, value));
+    }
+    else
+    {
+        List<string> peopleToDouble = people.FindAll(CheckingNames(filter, value));
+
+        foreach (string person in peopleToDouble)
+        {
+            int index = people.FindIndex(p => p == person);
+
+            people.Insert(index, person);
+        }
+    }
+}
+
+if (people.Any())
+{
+    Console.WriteLine($"{string.Join(", ", people)} are going to the party!");
+}
+else
+{
+    Console.WriteLine("Nobody is going to the party!");
+}
+
+static Predicate<string> CheckingNames(string filter, string value)
+{
+    switch (filter)
+    {
+        case "StartsWith": return n => n.StartsWith(value);
+        case "EndsWith": return n => n.EndsWith(value);
+        case "Length": return n => n.Length == int.Parse(value);
+        default:
+            return null;
+    }
+}
