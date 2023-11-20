c0440298008eb31d81c4d7c4c8f59a8a37639f0e
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-20 09:11:58 +0200

    Create Program.cs

---
+++ Functional Programming - EXERCISE/07. Predicate For Names/Program.cs
@@ -0,0 +1,24 @@
+﻿int nameLength = int.Parse(Console.ReadLine());
+
+List<string> names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+Func<List<string>, Predicate<string>, List<string>> filter = (names, match) =>
+{
+    List<string> result = new List<string>();
+
+    foreach (var name in names)
+    {
+        if (match(name))
+        {
+            result.Add(name);
+        }
+    }
+
+    return result;
+};
+
+names = filter(names, name => name.Length <= nameLength);
+
+Console.WriteLine(string.Join(Environment.NewLine, names));
