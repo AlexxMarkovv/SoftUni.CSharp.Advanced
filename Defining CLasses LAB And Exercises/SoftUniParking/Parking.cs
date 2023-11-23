7ac34f3eba9f989cdc6ac12350d5a2ee8842d9da
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-23 19:22:58 +0200

    AddedTask

---
+++ Defining CLasses LAB And Exercises/SoftUniParking/Parking.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        private Dictionary<string, Car> cars;
+        private int capacity;
+
+        public Parking(int capacity)
+        {
+            this.capacity = capacity;
+            cars = new Dictionary<string, Car>();
+        }
+
+        public int Count { get { return this.cars.Count; } }
+
+        public string AddCar(Car car)
+        {
+            if (cars.ContainsKey(car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+
+            if (Count == capacity)
+            {
+                return "Parking is full!";
+            }
+
+            this.cars.Add(car.RegistrationNumber, car);
+
+            return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+        }
+
+        public Car GetCar(string registrationNumber)
+        {
+            return cars[registrationNumber];
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            if (!cars.ContainsKey(registrationNumber))
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+
+            cars.Remove(registrationNumber);
+
+            return $"Successfully removed {registrationNumber}";
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (var registrationNumber in registrationNumbers)
+            {
+                RemoveCar(registrationNumber);
+            }
+        }
+    }
+}
