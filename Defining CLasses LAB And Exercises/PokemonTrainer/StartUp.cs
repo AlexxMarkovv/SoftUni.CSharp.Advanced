a7fe4972368ab0172f073ad5e06fd7036a81528e
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-23 19:23:37 +0200

    AddedTask

---
+++ Defining CLasses LAB And Exercises/PokemonTrainer/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace PokemonTrainer;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        List<Trainer> trainers = new();
+
+        string command;
+        while ((command = Console.ReadLine()) != "Tournament")
+        {
+            string[] cmndArrgs = command
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Trainer trainer = trainers.SingleOrDefault(t => t.Name == cmndArrgs[0]);
+
+            if (trainer == null)
+            {
+                trainer = new(cmndArrgs[0]);
+                trainer.Pokemons
+                    .Add(new Pokemon(cmndArrgs[1],
+                    cmndArrgs[2], int.Parse(cmndArrgs[3])));
+
+                trainers.Add(trainer);
+            }
+            else
+            {
+                trainer.Pokemons.Add(new Pokemon(cmndArrgs[1],
+                    cmndArrgs[2], int.Parse(cmndArrgs[3])));
+            }
+        }
+
+        string cmndType;
+        while ((cmndType = Console.ReadLine()) != "End")
+        {
+            string element = cmndType;
+
+            foreach (var trainer in trainers)
+            {
+                trainer.CheckPokemon(element);
+            }
+        }
+
+        foreach (var trainer in trainers.OrderByDescending(t => t.NumberOfBadges))
+        {
+            Console.WriteLine($"{trainer.Name} {trainer.NumberOfBadges} {trainer.Pokemons.Count}");
+        }
+    }
+}
---
+++ Defining CLasses LAB And Exercises/PokemonTrainer/Trainer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonTrainer
+{
+    public class Trainer
+    {
+        public Trainer(string name)
+        {
+            Name = name;
+            Pokemons = new();
+        }
+
+        public string Name { get; set; }
+        public int NumberOfBadges { get; set; }
+        public List<Pokemon> Pokemons { get; set; }
+
+        public void CheckPokemon(string element)
+        {
+            if (Pokemons.Any(p => p.Element == element))
+            {
+                NumberOfBadges++;
+            }
+            else
+            {
+                for (int i = 0; i < Pokemons.Count; i++)
+                {
+                    Pokemons[i].Health -= 10;
+
+                    if (Pokemons[i].Health <= 0)
+                    {
+                        Pokemons.Remove(Pokemons[i]);
+                    }
+                }
+            }
+        }
+    }
+}
