8c71af684f4c050a53135800c6fb95d68204081e
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-23 19:24:16 +0200

    AddedTask

---
+++ Defining CLasses LAB And Exercises/DefiningClasses/Family.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    internal class Family
+    {
+        private List<Person> members;
+
+        public Family()
+        {
+            this.Members = new List<Person>();
+        }
+
+        public List<Person> Members
+        {
+            get { return members; }
+            set { members = value; }
+        }
+
+        public void AddMember(Person member)
+        {
+             members.Add(member);
+        }
+
+        public Person GetOldestMember(List<Person> members)
+        {
+            return members.MaxBy(m => m.Age);
+        }
+    }
+}
---
+++ Defining CLasses LAB And Exercises/DefiningClasses/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Family family = new();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personInfo = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+
+                family.AddMember(new Person(name, age));
+            }
+
+            family.Members = family.Members.Where(m => m.Age > 30)
+                .OrderBy(m => m.Name).ToList();
+
+            foreach (var member in family.Members)
+            {
+                Console.WriteLine($"{member.Name} - {member.Age}");
+            }
+        }
+    }
+}
