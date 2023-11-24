6b694226ef98b85f104ae3e6fdcf3e9efa661db3
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-24 10:24:59 +0200

    Task

---
+++ Defining CLasses LAB And Exercises/07. Raw Data/Tire.cs
@@ -0,0 +1,13 @@
+﻿namespace RawData;
+
+public class Tyre
+{
+    public Tyre(double pressure, int age)
+    {
+        Pressure = pressure;
+        Age = age;
+    }
+
+    public double Pressure { get; set; }
+    public int Age { get; set; }
+}
