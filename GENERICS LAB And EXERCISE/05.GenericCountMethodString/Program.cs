0924e326cc14de91fcfb01626db3b08de657e68b
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-25 12:10:15 +0200

    Create Program.cs

---
+++ GENERICS LAB And EXERCISE/05.GenericCountMethodString/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace GenericCountMethodString;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int elCount = int.Parse(Console.ReadLine());
+
+        Box<double> box = new();
+
+        for (int i = 0; i < elCount; i++)
+        {
+            double item = double.Parse(Console.ReadLine());
+
+            box.Add(item);
+        }
+
+        double valueToCompare = double.Parse(Console.ReadLine());
+
+        int count = box.CountElementsGreaterThanValue(valueToCompare);
+        Console.WriteLine(count);
+    }
+}
