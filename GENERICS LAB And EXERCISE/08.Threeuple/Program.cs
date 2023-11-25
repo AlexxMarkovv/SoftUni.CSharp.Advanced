ca8872258b7f55cf221ca405265b149e4471536f
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-25 12:11:19 +0200

    Create Program.cs

---
+++ GENERICS LAB And EXERCISE/08.Threeuple/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Threeuple;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string[] personTokens = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        string[] drinkTokens = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        string[] numbersTokens = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        CustomThreeuple<string, string, string> nameAdress =
+            new($"{personTokens[0]} {personTokens[1]}",
+            personTokens[2], string.Join(" ", personTokens[3..]));
+
+        CustomThreeuple<string, int, bool> nameBeer =
+            new(drinkTokens[0], int.Parse(drinkTokens[1]), drinkTokens[2] == "drunk");
+
+        CustomThreeuple<string, double, string> numbers =
+            new(numbersTokens[0], double.Parse(numbersTokens[1]), numbersTokens[2]);
+
+        Console.WriteLine(nameAdress);
+        Console.WriteLine(nameBeer);
+        Console.WriteLine(numbers);
+
+    }
+}
