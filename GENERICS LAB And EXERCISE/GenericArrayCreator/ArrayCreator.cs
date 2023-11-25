ba38c3fc90fa979687db0c7eae1f1e0b8e77e316
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-25 12:10:02 +0200

    Create ArrayCreator.cs

---
+++ GENERICS LAB And EXERCISE/GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+        public static T[] Create<T>(int length, T value)
+        {
+            T[] array = new T[length];
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = value;
+            }
+
+            return array;
+        }
+    }
+}
