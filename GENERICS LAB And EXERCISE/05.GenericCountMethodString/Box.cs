26258423aa3f055dd208432b8eac9dd0d244cc48
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-25 12:08:12 +0200

    Create Box.cs

---
+++ GENERICS LAB And EXERCISE/05.GenericCountMethodString/Box.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCountMethodString;
+
+public class Box<T> where T : IComparable<T>
+{
+    private List<T> list;
+
+    public Box()
+    {
+        list = new List<T>();
+    }
+
+    public void Add(T item)
+    {
+        list.Add(item);
+    }
+
+    public int CountElementsGreaterThanValue(T value)
+    {
+        int count = 0;
+        foreach (T item in list)
+        {
+            if (value.CompareTo(item) == -1)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
