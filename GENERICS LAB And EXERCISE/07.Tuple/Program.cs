850d7c430d880c41f9003cc47bc36105af2018ae
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-25 12:10:46 +0200

    Create Program.cs

---
+++ GENERICS LAB And EXERCISE/07.Tuple/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Tuple;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //CustomTuple<int, int> customTuple = new(1, 2);
+
+
+        string[] personTokens = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        string[] drinkTokens = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        string[] numbersTokens = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        CustomTuple<string, string> nameAdress =
+            new($"{personTokens[0]} {personTokens[1]}", personTokens[2]);
+
+        CustomTuple<string, int> nameBeer = new(drinkTokens[0], int.Parse(drinkTokens[1]));
+
+        CustomTuple<int, double> numbers =
+            new(int.Parse(numbersTokens[0]), double.Parse(numbersTokens[1]));
+
+        Console.WriteLine(nameAdress);
+        Console.WriteLine(nameBeer);
+        Console.WriteLine(numbers);
+    }
+}
