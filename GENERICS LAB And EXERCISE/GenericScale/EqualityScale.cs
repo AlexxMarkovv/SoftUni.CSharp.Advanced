3e205fcd48f2ba13bb2ed16335c8e0a2e5c952ed
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-27 16:38:34 +0200

    Create EqualityScale.cs

---
+++ GENERICS LAB And EXERCISE/GenericScale/EqualityScale.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericScale;
+
+public class EqualityScale<T>
+{
+    public EqualityScale(T left, T right)
+    {
+        Left = left; Right = right;
+    }
+
+    public T Left { get; set; }
+    public T Right { get; set; }
+
+    public bool AreEqual()
+    {
+        return Left.Equals(Right);
+    }
+}
