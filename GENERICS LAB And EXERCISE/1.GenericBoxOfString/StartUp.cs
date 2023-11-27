a1af1691e8311001585a54e1f8474ce8fe9ed8cf
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-27 16:37:20 +0200

    Create StartUp.cs

---
+++ GENERICS LAB And EXERCISE/1.GenericBoxOfString/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace GenericBoxOfString;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Box<int> box = new();
+
+        for (int i = 0; i < n; i++)
+        {
+            int item = int.Parse(Console.ReadLine());
+
+            box.Add(item);
+        }
+
+        int[] indeces = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        box.SwapElements(indeces[0], indeces[1]);
+
+        Console.WriteLine(box);
+    }
+}
