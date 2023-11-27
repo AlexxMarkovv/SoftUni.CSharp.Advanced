21100effa15da346582e170a5128a93d5c67c6e0
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-27 16:38:45 +0200

    Create StartUp.cs

---
+++ GENERICS LAB And EXERCISE/GenericScale/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Immutable;
+
+namespace GenericScale;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        EqualityScale<int> equalityScale = new(5 ,5);
+
+        Console.WriteLine(equalityScale.AreEqual());
+    }
+}
