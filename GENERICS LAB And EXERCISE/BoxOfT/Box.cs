0178e4bc7dc7995c2fcebdcfae5e5f74cdba534e
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-27 16:37:39 +0200

    Create Box.cs

---
+++ GENERICS LAB And EXERCISE/BoxOfT/Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT;
+
+public class Box<T>
+{
+    private List<T> items;
+
+    public Box()
+    {
+        items = new List<T>();
+    }
+
+    public void Add(T element)
+    {
+        items.Add(element);
+    }
+
+    public T Remove()
+    {
+        T firstRlement = items[items.Count - 1];
+        items.RemoveAt(items.Count - 1);
+        return firstRlement;
+    }
+
+    public int Count
+    {
+        get { return items.Count; }
+    }
+
+}
