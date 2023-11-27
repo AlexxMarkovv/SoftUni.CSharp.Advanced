9e9b659e97932a16c5e6f55640426d5c66e04c81
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-27 16:37:07 +0200

    Create Box.cs

---
+++ GENERICS LAB And EXERCISE/1.GenericBoxOfString/Box.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxOfString;
+
+public class Box<T>
+{
+    private List<T> list;
+
+    public Box()
+    {
+        list = new List<T>();
+    }
+
+    public void Add(T item)
+    {
+        list.Add(item);
+    }
+
+ 
+    public List<T> SwapElements(int firstIndex, int secondIndex)
+    {
+        T firstItem = list.ElementAt(firstIndex);
+        list[firstIndex] = list[secondIndex];
+        list[secondIndex] = firstItem;
+
+        //(list[firstIndex], list[secondIndex]) = (list[secondIndex], list[firstIndex]);
+
+        return list;
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        foreach (T item in list)
+        {
+            sb.AppendLine($"{typeof(T)}: {item}");
+        }
+        
+        return sb.ToString().TrimEnd();
+    }
+}
