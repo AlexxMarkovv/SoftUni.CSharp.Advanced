b42310262a1200fe65e0ad518eb23209881f4d55
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-28 17:54:48 +0200

    Create Library.cs

---
+++ Iterators and Comparators - LAB and EXERCISE/IteratorsAndComparators/Library.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = books.ToList();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(books.OrderBy(b => b, new BookComparator()).ToList());
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private List<Book> books;
+            private int index = -1;
+
+            public LibraryIterator(List<Book> books)
+            {
+                this.books = books;
+            }
+
+            public Book Current => books[this.index];
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose() { }
+
+            public bool MoveNext() => ++index < books.Count;
+
+            public void Reset() { index = -1; }
+        }
+    }
+}
