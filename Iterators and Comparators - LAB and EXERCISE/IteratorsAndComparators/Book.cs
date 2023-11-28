8fdcec0f02c69f059c7134f10e8e5d4edbeb5282
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-11-28 17:54:17 +0200

    Create Book.cs

---
+++ Iterators and Comparators - LAB and EXERCISE/IteratorsAndComparators/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators;
+
+public class Book : IComparable<Book>
+{
+    public Book(string title, int year, params string[] authors)
+    {
+        Title = title;
+        Year = year;
+        Authors = authors.ToList();
+    }
+
+    public string Title { get; set; }
+    public int Year { get; set; }
+    public List<string> Authors { get; set; }
+
+    public int CompareTo(Book other)
+    {
+        int result = Year.CompareTo(other.Year);
+        if (result == 0)
+        {
+            return Title.CompareTo(other.Title);
+        }
+
+        return result;
+    }
+
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.Append($"{Title} - {Year}");
+
+        return sb.ToString().TrimEnd();
+    }
+}
