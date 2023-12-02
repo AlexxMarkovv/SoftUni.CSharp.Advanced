ae43abd3e67538d5b03df165bd39647b4a0283a9
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-12-02 12:36:56 +0200

    Create Shoe.cs

---
+++ C# ADVANCED - EXAM PREPARATION/03. Shoe Store/Shoe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoeStore
+{
+    public class Shoe
+    {
+        public Shoe(string brand, string type, double size, string material)
+        {
+            Brand = brand;
+            Type = type;
+            Size = size;
+            Material = material;
+        }
+
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public double Size { get; set; }
+        public string Material { get; set; }
+
+        public override string ToString()
+        {
+            return $"Size {Size}, {Material} {Brand} {Type} shoe.".ToString();
+        }
+    }
+}
