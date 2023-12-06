f1db418eb8ed7f4ab208a25d9e4f63a83464fe6b
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-12-06 18:10:43 +0200

    Create StartUp.cs

---
+++ C# ADVANCED - EXAM PREPARATION/03.ComputerArchitecture/StartUp.cs
@@ -0,0 +1,58 @@
+﻿namespace ComputerArchitecture;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        // Initialize the repository
+
+        Computer computer = new Computer("Gaming Serioux", 4);
+
+        // Initialize entity
+
+        CPU cpu = new CPU("AMD Ryzen 5", 6, 3.7);
+
+        // Print CPU
+
+        Console.WriteLine(cpu);
+
+        // AMD Ryzen 5 CPU:
+
+        // Cores: 6
+
+        // Frequency: 3.7 GHz
+
+        // Add CPU
+
+        computer.Add(cpu);
+
+        // Remove CPU
+
+        Console.WriteLine(computer.Remove("Intel Core i5"));
+
+        // False
+
+        CPU secondCPU = new CPU("Intel Core i7", 8, 4);
+
+        CPU thirdCPU = new CPU("Intel Core i5", 8, 3.9);
+
+        // Add CPU
+
+        computer.Add(secondCPU);
+
+        computer.Add(thirdCPU);
+
+        CPU mostPowerful = computer.MostPowerful();
+
+        Console.WriteLine(mostPowerful);///////////
+
+        CPU receivedCPU = computer.GetCPU("Intel Core i5");
+        Console.WriteLine(receivedCPU);
+
+
+        Console.WriteLine(computer.Count);
+        Console.WriteLine(computer.Remove("Intel Core i5"));
+
+        Console.WriteLine(computer.Report());
+    }
+}
