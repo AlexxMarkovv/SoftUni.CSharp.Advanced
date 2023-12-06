ca0c7d22d10279bd68cef3daf74da251a3ee7145
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-12-06 18:09:14 +0200

    Create Player.cs

---
+++ C# ADVANCED - EXAM PREPARATION/03.Basketball/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basketball
+{
+    public class Player
+    {
+        public Player(string name, string position, double rating, int games)
+        {
+            Name = name;
+            Position = position;
+            Rating = rating;
+            Games = games;
+        }
+
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public double Rating { get; set; }
+        public int Games { get; set; }
+        public bool Retired { get; set; }
+    }
+}
