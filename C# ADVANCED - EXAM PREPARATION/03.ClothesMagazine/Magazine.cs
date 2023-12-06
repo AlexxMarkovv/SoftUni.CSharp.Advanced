e5ee84520f3f18f6f40174dc75d65254068fcc3e
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-12-06 18:09:51 +0200

    Create Magazine.cs

---
+++ C# ADVANCED - EXAM PREPARATION/03.ClothesMagazine/Magazine.cs
@@ -0,0 +1,67 @@
+﻿
+using System.Text;
+
+namespace ClothesMagazine
+{
+    public class Magazine
+    {
+        public Magazine(string type, int capacity)
+        {
+            Type = type;
+            Capacity = capacity;
+            Clothes = new List<Cloth>();
+        }
+
+        public string Type { get; set; }
+        public int Capacity { get; set; }
+        public List<Cloth> Clothes { get; set; }
+
+        public void AddCloth(Cloth cloth)
+        {
+            if (Clothes.Count < Capacity)
+            {
+                Clothes.Add(cloth);
+            }
+        }
+
+        public bool RemoveCloth(string color)
+        {
+            Cloth cloth = Clothes.FirstOrDefault(c => c.Color == color);
+            bool isRemoved = Clothes.Remove(cloth);
+
+            return isRemoved;
+        }
+
+        public Cloth GetSmallestCloth() =>
+            Clothes.OrderBy(x => x.Size)
+                   .FirstOrDefault();
+
+        public Cloth GetCloth(string color)
+        {
+            Cloth cloth = null;
+
+            cloth = Clothes.FirstOrDefault(c => c.Color == color);
+
+            return cloth;
+        }
+
+        public int GetClothCount()
+        {
+            return Clothes.Count;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{Type} magazine contains: ");
+
+            foreach (var item in Clothes.OrderBy(x => x.Size))
+            {
+                sb.AppendLine(item.ToString());
+            }
+            
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
